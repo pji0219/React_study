99c13cdff16c8cf1cdb2206744460329587bc31b
park jong ik <pji219@naver.com>
2020-08-14 22:05:57 +0900

    나의 첫번째 컴포넌트

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+// 리액트 컴포넌트
+// 컴포넌트는 일종의 UI 조각
+import React from 'react'; // 리액트를 불러와서 사용하겠다는 의미
+
+function Hello() {
+    return <div>안녕하세요!</div>;
+}
+
+export default Hello; // Hello라는 컴포넌트를 내보내겠다는 의미
