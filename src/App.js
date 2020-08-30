7de595c1a738b5173860514ed217355bb470bff2
park jong ik <pji219@naver.com>
2020-08-30 22:34:15 +0900

    userState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 // App.js
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 function App() {
    return (
-     <Wrapper>
-       <Hello name="react" color="blue" isSpecial={true}/> {/* 자바스크립트 값이라서 { }를 써 주어야함 */}
-       <Hello color="pink"/>
-     </Wrapper>
-    );
+     <Counter/>
+     );
 }
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'; // 동적인 상태로 하려면 useStats 함수를 불러와 줘야 한다.
+
+function Counter() {
+    const [number, setNumber] = useState(0); // 넘버라는 상태를 만들건데 그 기본값을 0으로 하겠다, 셋넘버는 이 상태를 바꿔주는 함수
+
+    const onIncrease = () => {
+        setNumber(number + 1);
+    };
+      const onDecrease = () => {
+        setNumber(number - 1);
+    };
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+1</button> {/* 함수를 호출하면 안되고 이름만 넣어줘야한다. */}
+            <button onClick={onDecrease}>-1</button>
+        </div>
+    )
+}
+
+export default Counter;
