396c0b7674890d6a7644689056d9efbdb68ff0d1
park jong ik <pji219@naver.com>
2020-10-09 22:57:03 +0900

    클래스 카운터2

--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 
 class ClassCounter extends Component {
-
-    handleIncrease() { // state로 사용하기 위한 커스텀 메서드를 만들수 있음
-        console.log('increase');
+    constructor(props) { // 상태관리
+        super(props);
+        this.state = { // 상태, state의 형태는 무조건 객체 형태여야 한다.
+            counter: 0 // 초기값
+        };
     }
 
-    handleDecrease() {
-        console.log('decrease');
+    handleIncrease = () => { // state로 사용하기 위한 커스텀 메서드를 만들수 있음, 화살표 함수로 해야 이벤트로 연결 해줄때 인스턴스와 this가 안 끊김
+        this.setState({ // 상태를 바꾸기 위한 함수
+            counter: this.state.counter + 1 // counter 현재값에 +1
+        });
+    }
+
+    handleDecrease = () => {
+       this.setState({
+            counter: this.state.counter - 1
+        })
     }
 
     render() {
         return (
             <div>
-                <h1>0</h1>
+                <h1>{this.state.counter}</h1>
                 <button onClick={this.handleIncrease}>+1</button>
                 <button onClick={this.handleDecrease}>-1</button>
             </div>
